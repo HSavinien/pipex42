677b7f91cab6a44aa59e40a2fb563cc73a8ddd36
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-07-05 18:49:18 +0200

    removed debug messages

--- sub_process.c
+++ sub_process.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:54:02 by tmongell          #+#    #+#             */
-/*   Updated: 2022/07/05 17:47:38 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/07/05 18:48:20 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ char	**get_path(char **env)
 		i ++;
 	if (!env)
 		error ("environment variable PATH can't be located");
-	dprintf(2, "found path : %s\n", env[i]);//debug
 	path = ft_split(ft_strchr(env[i], '=') + 1, ':');
 	if (!path)
 		error("problem with env var PATH");
@@ -38,21 +37,16 @@ void	do_cmd(char	*fct, char **env)
 	int		i;
 
 	pid = fork();
-	dprintf(2, "entering fct do_cmd\n");//debug
 	if (!pid)
 	{
-		dprintf(2, "entering child process\n");//debug
 		fct = extract_args(fct, &args);
 		path = get_path(env);
-		dprintf(2, "funct, args, and path env var extracted\n");//debug
 		i = 0;
 		execve(fct, args, env);
 		while (path[i])
 		{
-			dprintf(2, "\e[1mloop\e[0m\n");//debug
 			cmd_path = ft_strjoin(path[i ++], "/");
 			cmd_path = ft_strjoin(cmd_path, fct);
-			dprintf(2, "trying cmd %s\n", cmd_path);//debug
 			execve(cmd_path, args, env);
 			free(cmd_path);
 		}
@@ -61,6 +55,5 @@ void	do_cmd(char	*fct, char **env)
 	else
 	{
 		waitpid(pid, &i, 0);
-		dprintf(2, "back to parent process\n");
 	}
 }
