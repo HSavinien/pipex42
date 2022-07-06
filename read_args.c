4a8f73490bfca34bd0102b28af3b74303c11180c
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-07-06 14:21:15 +0200

    corrected characters remaining if outfile was alredy written, and bigger than the result of pipex

--- read_args.c
+++ read_args.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 14:38:46 by tmongell          #+#    #+#             */
-/*   Updated: 2022/07/05 19:05:39 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/07/06 14:06:58 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	read_args(int ac, char **av, int *fd_in, int *fd_out)
 	if (ac != 5)
 		exit(ft_printf("usage : %s <infile> <cmd1> <cmd2> <outfile>\n", av[0]));
 	*fd_in = open(av[1], O_RDONLY);
-	*fd_out = open(av[4], O_WRONLY | O_CREAT, 0644);
+	*fd_out = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (*fd_in < 0)
 		exit(ft_printf("error : cannot read file %s\n", av[1]));
 	if (*fd_out < 0)
